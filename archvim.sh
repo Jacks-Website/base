83bc02d678ae274ee0e83c939faaaf7e737f7c4c
Jack Murphy <jack@jack.engineering>
2020-04-25 18:34:16 -0400

    Update and rename archvim-install.sh to archvim.sh

--- archvim.sh
+++ archvim.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 cd ~/
-pacman -S python3 python-pip python-isort python-black flake8 python-jedi git --noconfirm
+pacman -S python3 python-pip python-isort python-black flake8 python-jedi git vim --noconfirm
 git clone https://github.com/Jack-alope/vim.git
 mv ~/vim/.vim ~/
 mv ~/vim/.vimrc ~/
 rm -r vim
+echo "Done"
