e5fee23491a6f68dbd19d80cf5d46fc28161671c
Jack Murphy <jack@jack.engineering>
2020-04-25 20:13:23 -0400

    Create ubuntuvim.sh

---
+++ ubuntuvim.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ~/
+sudo apt-get update
+sudo apt install black isort flake8 python3-jedi git vim -y
+git clone https://github.com/Jack-alope/vim.git
+mv ~/vim/.vim ~/
+mv ~/vim/.vimrc ~/
+rm -r vim
+echo "Vim Configuration Complete"
