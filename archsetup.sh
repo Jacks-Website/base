7dca55fb8141a0698a9ed8bb2dcf46ebbbeec5ba
Jack Murphy <jack@jack.engineering>
2020-04-26 17:28:24 -0400

    Create archsetup.sh

---
+++ archsetup.sh
@@ -0,0 +1,15 @@
+  
+#!/bin/bash
+
+cd ~/
+pacman -S python3 python-pip python-isort python-black flake8 python-jedi git vim which fish tmux --noconfirm
+git clone https://github.com/Jack-alope/vim.git
+mv ~/vim/.vim ~/
+mv ~/vim/.vimrc ~/
+rm -r vim
+echo "Vim Configuration Complete"
+curl -L https://get.oh-my.fish | fish
+echo "OMF Installed"
+echo "/usr/sbin/fish" >> /etc/shells
+chsh -s /usr/sbin/fish
+echo "Default Shell Changed to Fish"
