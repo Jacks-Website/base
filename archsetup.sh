d1aef54ad8ed4945d553a371d8df136fc468a994
Jack Murphy <jack@jack.engineering>
2020-04-27 09:44:04 -0400

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -3,11 +3,13 @@
 
 cd ~/
 pacman -Sy
-pacman -S python3 python-pip python-isort python-black flake8 python-jedi git which fish vim tmux --noconfirm
+pacman -S which git fish
+curl -L https://get.oh-my.fish | fish
+pacman -S python3 python-pip python-isort python-black flake8 python-jedi tmux
 git clone https://github.com/Jack-alope/vim.git
 mv ~/vim/.vim ~/
 mv ~/vim/.vimrc ~/
 rm -r vim
 echo "Vim Configuration Complete"
-curl -L https://get.oh-my.fish | fish
+
 echo "OMF Installed"
