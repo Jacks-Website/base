748e05085357d47c26a7931b8614b453f3b0d5a6
Jack Murphy <jack@jack.engineering>
2020-04-26 20:07:43 -0400

    Update archvim.sh

--- archvim.sh
+++ archvim.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 cd ~/
-pacman -S python3 python-pip python-isort python-black flake8 python-jedi git vim --noconfirm
+pacman -S python3 python-pip python-isort python-black flake8 python-jedi git vim which tmux --noconfirm
 git clone https://github.com/Jack-alope/vim.git
 mv ~/vim/.vim ~/
 mv ~/vim/.vimrc ~/
 rm -r vim
-echo "Vim Configuration Complete"
