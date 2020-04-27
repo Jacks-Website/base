5d3b2b64eaf3435d46e47ffddee4be8176340092
Jack Murphy <jack@jack.engineering>
2020-04-27 10:59:53 -0400

    Delete vim.sh

--- archvim/vim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ~/
-pacman -Sy
-pacman -S python3 python-pip python-isort python-black flake8 python-jedi vim tmux
-git clone https://github.com/Jack-alope/vim.git
-mv ~/vim/.vim ~/
-mv ~/vim/.vimrc ~/
-rm -r vim
