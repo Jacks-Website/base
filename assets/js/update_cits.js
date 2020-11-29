92fe4a4d135da7e4d34d2ff7ad56b0aeb99a52c7
Jack <me@jackmurphy.nyc>
2020-11-29 19:50:57 +0000

    minified

--- assets/js/update_cits.js
+++
@@ -1,5 +0,0 @@
-$.getJSON('https://raw.githubusercontent.com/Jack-alope/google-scholar-scraper/main/citation_stats.json', function(data) {
-        var text = `Citations: <strong>${data.total}</strong> | h-index: <strong>${data.h_index}</strong> | i10-index: <strong>${data.i10_index}</strong>`
-
-        total_cit.innerHTML = text; 
-});
