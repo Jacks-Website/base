15761b820349244e056c1b2d521511f31a63c83b
Jack <jack@mrph.dev>
2020-12-28 16:54:43 +0000

    smooth

--- assets/js/update_cits.js
+++ assets/js/update_cits.js
@@ -1,6 +1,10 @@
 $.getJSON('https://raw.githubusercontent.com/Jack-alope/google-scholar-stats/main/Jack%20F.%20Murphy.json', function(data) {
+    console.log('hello')
         data = JSON.parse(data)
         var text = `Citations: <strong>${data.citedby}</strong> | h-index: <strong>${data.hindex}</strong> | i10-index: <strong>${data.i10index}</strong>`
 
         total_cit.innerHTML = text; 
+        console.log(data.cites_per_year)
+        var container = document.getElementById('conta');
+container.insertBefore(document.createElement("p"), container.firstChild);
 });
